9a469713ed1042ee3426b3cdf32ecf1743cdea1d
Paul Zellmer <paulzellmer9@gmail.com>
2014-11-04 23:31:02 -0800

    heroku ready

--- quoter/settings.py
+++ quoter/settings.py
@@ -60,16 +60,16 @@ WSGI_APPLICATION = 'quoter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'quoter',
-        'USER': 'postgres',
-        'PASSWORD': 'hyperion',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'quoter',
+#         'USER': 'postgres',
+#         'PASSWORD': 'hyperion',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
@@ -119,5 +119,5 @@ TEMPLATE_DIRS = (
 LOGIN_REDIRECT_URL = '/quotes'
 LOGIN_URL = '/'
 
-# DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config()
 
