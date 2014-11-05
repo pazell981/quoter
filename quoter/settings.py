e5f504177d4916ed2ccec172924292751354891a
Paul Zellmer <paulzellmer9@gmail.com>
2014-11-05 15:18:48 -0800

    static files fix

--- quoter/settings.py
+++ quoter/settings.py
@@ -82,7 +82,7 @@ USE_TZ = True
 MEDIA_ROOT = local_path('../media/')
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+STATIC_ROOT = os.path.join(BASE_DIR, '../staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
